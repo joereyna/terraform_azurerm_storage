00cb4df2d63afd81dacc9e86160a6ad32c341a31
Joe Reyna <jreyna@gh.st>
2023-02-09 16:47:37 -0800

    Fix resource group reference.

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "resource_group" {
 #============================================================
 resource "azurerm_storage_account" "storage_account" {
   name                      = var.storage_account_name
-  resource_group_name       = var.resource_group_name
+  resource_group_name       = azurerm_resource_group.resource_group.name
   location                  = var.storage_region_name
   account_kind              = var.storage_account_type
   account_tier              = var.storage_account_tier
